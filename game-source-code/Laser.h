677cb481e5243b97d19cf25fcb8551d5279eaed8
Adam Blumenthal <adamblumenthal98@gmail.com>
2021-09-30 02:07:06 +0200

    Laser Header

---
+++ game-source-code/Laser.h
@@ -0,0 +1,43 @@
+#ifndef LASER_H
+#define LASER_H
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "BugBlaster.h"
+
+
+class Laser
+{
+    public:
+        Laser(sf::FloatRect BugBounds);
+        virtual ~Laser();
+
+        //Varibles
+        float MoveSpeed;
+
+//functions
+        void LaserMove();
+        void update(sf::RenderTarget* target);
+        void render(sf::RenderTarget* target);
+        void SetPositionLaser(sf::FloatRect BugBounds);
+
+        void CheckCollisionWindow(sf::RenderTarget* target);
+
+        sf::FloatRect GetLaserPosition();
+
+
+
+
+    private:
+        sf::RectangleShape laser;
+
+        void createVarible();
+        void createLaser();
+
+
+
+};
+
+#endif // LASER_H
