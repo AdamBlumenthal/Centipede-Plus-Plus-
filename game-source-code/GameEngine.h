d161a379c3c3e8c2629cf2803b2379d72c9ba559
ZunaidValodia <1624885@students.wits.ac.za>
2021-11-02 16:01:35 +0200

    Doxygen comments for Game Class

--- game-source-code/GameEngine.h
+++ game-source-code/GameEngine.h
@@ -13,6 +13,15 @@
 #include "Bomb.h"
 #include <memory>
 
+/** \file Mushroom.h
+    \brief Contains code for constructing the Game class
+ */
+
+/** \class Game
+    \brief A class that holds the game objects and controls and renders them
+ */
+
+
 
 class Game
 {
@@ -20,25 +29,51 @@ public:
 
 
     //Constructor
+    /** \brief The Constructor for the Game class.
+    This also initializes the Starting objects and constants
+    */
     Game();
+    /** \brief The Destructor for the Game class.
+    */
     virtual ~Game();
 
     //Checks
     const bool IsRunning() const;
 
     //Functions
-    void createVarible();
-    void createWindow();
-    void update();
-    void render();
-    void UpdateEvent();
-    void ShootLaser();
-    void SpawnFlea();
-    void SpawnMushroomField();
+    /** \brief initializes the Starting objects in the game and constants
 
+    */
+    void createVarible();
+    /** \brief Sets the conditions and parameters of the render window
+
+    */
+    void createWindow();
+    /** \brief Updates the all the ojects in the game based on collisions and movments
+    */
+    void update();
+    /** \brief Renders all of the objects in the render window
+
+    */
+    void render();
+    /** \brief Polls to determine the new key inputs
+
+    */
+    void UpdateEvent();
+    /** \brief Creates a Laser object to simulate the player shooting
+
+    */
+    void ShootLaser();
+    /** \brief Creates a Flea object at the top of the screen
+    */
+    void SpawnFlea();
+    /** \brief Creates a vector full of randomly placed Mushroom Objects
+    */
+    void SpawnMushroomField();
+/** \brief Creates a Bomb Ojects based on a random chance
+    */
     void SpawnBomb();
 
-    void SelfCollision();
 
 private:
 
@@ -78,15 +113,8 @@ private:
     sf::RectangleShape BotLine;
 
     CollisionControl Collision;
-    //BugBlaster BugB;//BugBlaster object
-    //std::vector <Laser> laser;//Laser object
-    //std::vector <Segment> segments;//Centipede
-    //std::vector <Segment> heads;//Independent centipedes
-    //std::vector <Mushroom*> Mush;
-    //std::vector <Flea*> flea;//Flea object
-    //std::vector<int>headCount;
-    //std::vector<Centipede> centipedes;
-    std::shared_ptr<BugBlaster> BugB;//=std::make_shared<BugBlaster>();
+
+    std::shared_ptr<BugBlaster> BugB;
     std::vector<std::shared_ptr<Laser>> laser;
     std::vector<std::shared_ptr<Centipede>> centipedes;
     std::vector<std::shared_ptr<Mushroom>> Mush;
